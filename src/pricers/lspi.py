98ac89f953b1ee812b089b21c8a9a0673950b112
Кмито Глеб <kmito2004@gmail.com>
2025-04-23 07:53:16 +0300

    rainbow lspi

--- src/pricers/lspi.py
+++ src/pricers/lspi.py
@@ -1,8 +1,8 @@
 import numpy as np
 from typing import Optional
 from sklearn.preprocessing import StandardScaler
-from ..pricers.abstract_pricer import PricerAbstract
-from ..samplers.abstract_sampler import SamplerAbstract
+from pricers.abstract_pricer   import PricerAbstract
+from samplers.abstract_sampler import SamplerAbstract
 
 
 class LSPIPricer(PricerAbstract):
@@ -20,16 +20,21 @@ class LSPIPricer(PricerAbstract):
         self.sampler = sampler
 
     def _basis_functions(self, S: np.ndarray, K: float, t: np.ndarray, T: float) -> np.ndarray:
-        M = S / K
+        if S.shape[2] > 1: # rainbow случай
+            min_S = np.min(S, axis=2)  # (num_paths, num_times)
+        else: 
+            min_S = S[:, :, 0]  # (num_paths, num_times)
+        
+        M = min_S / K
         exp_term = np.exp(-M / 2.0)
 
-        phi = np.zeros((*S.shape, 7), dtype = float)
+        phi = np.zeros((S.shape[0], S.shape[1], 7), dtype=float)
         phi[:, :, 0] = 1.0
         phi[:, :, 1] = exp_term
         phi[:, :, 2] = exp_term * (1.0 - M)
         phi[:, :, 3] = exp_term * (1.0 - 2.0 * M + 0.5 * M * M)
         phi[:, :, 4] = np.sin(np.pi * (T - t) / (2.0 * T))
-        phi[:, :, 5] = np.log(np.maximum(T - t, 1e-10)) # 1e-10 чтобы не было log(0)
+        phi[:, :, 5] = np.log(np.maximum(T - t, 1e-10))
         phi[:, :, 6] = (t / T) ** 2
 
         scaler = StandardScaler()
@@ -37,15 +42,13 @@ class LSPIPricer(PricerAbstract):
         phi_scaled = scaler.fit_transform(phi_reshaped)
         phi_scaled = phi_scaled.reshape(phi.shape[0], phi.shape[1], 6)
 
-
         phi_final = np.zeros_like(phi)
         phi_final[:, :, 0] = phi[:, :, 0]
         phi_final[:, :, 1:] = phi_scaled
 
         return phi_final
 
-    def price(self, test=False, quiet=None) -> np.ndarray:
-
+    def price(self, test = False) -> np.ndarray:
         self.sampler.sample()
 
         K = self.sampler.strike
@@ -54,39 +57,34 @@ class LSPIPricer(PricerAbstract):
         T = time_grid[-1]
         dt = time_grid[1] - time_grid[0]
         gamma = np.exp(-r * dt)
-        S_paths = self.sampler.markov_state[:, :, 0]
+        S_paths = self.sampler.markov_state
         payoff_matrix = self.sampler.payoff
         disc_matrix = self.sampler.discount_factor
-        num_paths, num_times = S_paths.shape
+        num_paths, num_times = S_paths.shape[0], S_paths.shape[1]
         d = 7
 
         time_grid_expanded = np.tile(time_grid, (num_paths, 1))
         phi_all = self._basis_functions(S_paths, K, time_grid_expanded, T)
 
         if not test:
-
             w = np.zeros(d, dtype=float) if self.w is None else self.w.copy()
-
-            phi_curr_all = phi_all[:, :-1, :]# базисные ф-ии кроме терминального состояния, phi(s_i)
-            phi_next_all = phi_all[:, 1:, :]# базисные ф-ии кроме начального состояния, phi(s_i')
-
+            phi_curr_all = phi_all[:, :-1, :]
+            phi_next_all = phi_all[:, 1:, :]
 
             for it in range(self.iterations):
                 prev_w = w.copy()
                 A = np.zeros((d, d), dtype=float)
                 b = np.zeros(d, dtype=float)
 
+
                 # phi_next_all размера num_paths × (num_times-1) × d) умножается на w размера d
                 # и получается результат размера num_paths × (num_times-1)
                 # соответствует phi(s_i') * w для каждой траектории p и для каждого времени t
                 Q_cont_next = np.einsum('ptd,d->pt', phi_next_all, w)
-
                 Q_ex_next = disc_matrix[:, 1:] * payoff_matrix[:, 1:]
-
                 non_terminal_s_prime = np.tile(
                     (np.arange(num_times - 1) < num_times - 2)[np.newaxis, :], (num_paths, 1)
                 )
-
                 next_cont_cond = non_terminal_s_prime & (Q_cont_next >= Q_ex_next)
 
                 # diff_phi - это phi(s_i) - Indicator(C_1) * gamma * phi(s_i')
@@ -96,16 +94,14 @@ class LSPIPricer(PricerAbstract):
                 b += np.einsum('ptd,pt->d', phi_curr_all, (~next_cont_cond) * Q_ex_next)
 
                 A += self.lambda_reg * np.eye(d)
-                w, _, _, _ = np.linalg.lstsq(A, b, rcond = None)
+                w, _, _, _ = np.linalg.lstsq(A, b, rcond=None)
 
                 diff = np.linalg.norm(w - prev_w)
                 print(f"на {it}-ой итерации норма разности весов составила {round(diff, 4)}")
-                
                 if diff < self.tol:
                     break
             self.w = w.copy()
         else:
-
             w = self.w
 
         pv_payoffs = np.zeros(num_paths, dtype=float)
@@ -117,8 +113,7 @@ class LSPIPricer(PricerAbstract):
                     pv_payoffs[p] = disc_factor * payoff_now
                     break
                 Q_cont = np.dot(phi_all[p, t_idx], w)
-                continuation_value = Q_cont
-                if payoff_now >= continuation_value:
+                if payoff_now >= Q_cont:
                     pv_payoffs[p] = disc_factor * payoff_now
                     break
 
--- src/samplers/wiener_rainbow_put_option_sampler.py
+++ src/samplers/wiener_rainbow_put_option_sampler.py
@@ -1,30 +1,31 @@
 import numpy as np
 from tqdm.auto import tqdm
-from src.samplers.abstract_sampler import SamplerAbstract
+from .abstract_sampler import SamplerAbstract
 
 
 class WienerRainbowPutOptionSampler(SamplerAbstract):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.sigmas = kwargs.get("sigmas") # список волатильностей каждого
-        # актива (это радужный, поэтому их тут мб много)
-        self.strike = kwargs.get("strike")     # скаляр
-        self.r = kwargs.get("r", 0.05)          #  скаляр
-        self.dim = len(self.sigmas) # размерность радужного вектора
+        self.sigmas = kwargs.get("sigmas")
+        self.dim = len(self.sigmas)
+        self.strike = kwargs.get("strike")
+        self.asset0 = kwargs.get("asset0") 
+        self.r = kwargs.get("r")
 
     def sample(self) -> None:
         normals = self.random_state.normal(0, 1, (self.dim, self.cnt_trajectories, self.cnt_times - 1))
         self.markov_state = np.zeros((self.dim, self.cnt_trajectories, self.cnt_times), dtype=float)
         
         for i in range(self.dim):
-            for j in tqdm(range(self.cnt_trajectories), desc="WienerRainbow sampling"):
-                self.markov_state[i][j] = np.zeros(len(self.time_deltas) + 1, dtype=float)
-                self.markov_state[i][j][1:] = self.sigmas[i] * np.cumsum(normals[i][j] * np.sqrt(self.time_deltas))
+            self.markov_state[i, :, 0] = self.asset0[i]  # Установка начальной цены
+            for j in tqdm(range(self.cnt_trajectories), desc="WienerRainbow sampling with asset0"):
+                self.markov_state[i][j][1:] = self.asset0[i] * np.exp(
+                    (-0.5 * self.sigmas[i]**2) * self.time_deltas +
+                    self.sigmas[i] * np.cumsum(normals[i][j] * np.sqrt(self.time_deltas))
+                )
         
         self.payoff = np.clip(self.strike - np.min(self.markov_state, axis=0), 0, 1e20)
-        
         self.discount_factor = np.repeat(
             np.exp(-self.r * self.time_grid).reshape((1, -1)), self.cnt_trajectories, axis=0
         )
-
         self.markov_state = np.transpose(self.markov_state, (1, 2, 0))
